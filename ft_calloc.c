5fdfd1d09130b9061bf6e6541b84548ce541901a
yhubaren <yhubaren@student.42prague.com>
2026-08-22 17:26:23 +0200

    Part 1 compleated

---
+++ ft_calloc.c
@@ -0,0 +1,13 @@
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*tmp;
+
+	tmp = malloc(count * size);
+	if (tmp == 0)
+		return (NULL);
+	ft_bzero(tmp, count * size);
+	return (tmp);
+}
--- ft_strdup.c
+++ ft_strdup.c
@@ -3,16 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yhubaren <yhubaren@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 15:41:13 by yhubaren          #+#    #+#             */
-/*   Updated: 2026/08/22 15:42:52 by yhubaren         ###   ########.fr       */
+/*   Updated: 2026/08/22 16:47:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strdup(char src)
+char *ft_strdup(const char *src)
 {
-		
+	size_t	i;
+	char	*dst;
+
+	i = 0;
+	dst = malloc((sizeof(char)) * (ft_strlen(src) + 1));
+	if (dst == NULL)
+		return (NULL);
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
